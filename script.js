6cff71bb093c8399f819f3e41a104941754d843a
linkaixul <77321207+linkaixul@users.noreply.github.com>
2022-08-19 16:20:56 +0800

    Create Chinese and English version

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const open =document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const eng = document.getElementById('english');
+const switchBtn = document.querySelector('.chn')
+
 
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+// switch language mode - CHN -> ENG
+switchBtn.addEventListener('click', (e) =>{
+    let contact = document.getElementById('contact')
+    if(switchBtn.classList.contains('chn')){
+        // debugger;
+        switchBtn.classList.remove('chn')
+        e.target.innerHTML ='ENGLISH'
+        contact.innerHTML = '联系'
+        switchCHN('chn')
+    }else{
+        switchBtn.classList.add('chn')
+        e.target.innerHTML = '中文'
+        contact.innerHTML = 'Contact Me'
+        switchCHN('eng')
+    }
+})
+function switchCHN(language) {
+    let home = document.getElementById('fa-home')
+    let about = document.getElementById('fa-user-alt')
+    let contact = document.getElementById('fa-envelope')
+    if( language == 'chn'){
+        home.innerHTML = '主页';
+        about.innerHTML = '关于我';
+        about.href = 'about-me-chn.html';
+        contact.innerHTML = '联系';
+    }else if( language == 'eng'){
+        home.innerHTML = 'Home';
+        about.innerHTML = 'About';
+        about.href = 'about-me.html';
+        contact.innerHTML = 'Contact';
+    }
+
+}
